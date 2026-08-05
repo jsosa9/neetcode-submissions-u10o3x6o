fa6e1608477a89214fa3ca2b50a896f77d63b81c
Jose Sosa <109876904+jsosa9@users.noreply.github.com>
2026-08-04 21:54:31 -0400

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,39 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """
+        well we can use a hahsmap where 
+        the key is the original value fro mthe array 
+        the value of the hm is the index its ucrrently at 
+
+        before we add we check the current keys in the hm to see if we find a pair
+
+        """
+        hm = {}
+
+        for i, n in enumerate(nums):
+            """
+            Take the current value we're passing through and add it with every key in 
+            the hashmap 
+
+            if the total is equal to target reutnr both indicides else add the current      
+            value to the hm 
+            """
+            if len(hm) > 0:
+                # comapre the value with every value in the hm 
+                for k in hm:
+                    if n + k == target:
+                        return [hm[k], i]
+            hm[n] = i
+        """
+        for i, n in enumerate(nums):
+            hm[n] = i
+        
+        for n in nums:
+            for k in hm:
+                if len(hm) > 0:
+                    # comapre the value with every value in the hm 
+                        if n + k == target:
+                            return [hm[k], i]
+                        else:
+                            hm[n] = i
+        """
